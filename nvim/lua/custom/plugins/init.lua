033a5e7da3f14cd6504f99c79e78afc99430673f
Frank <25704248+rakuzen25@users.noreply.github.com>
2025-01-15 16:52:41 -0600

    feat: add remove-nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -168,4 +168,14 @@ return {
       })
     end,
   },
+  {
+    'amitds1997/remote-nvim.nvim',
+    version = '*', -- Pin to GitHub releases
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- For standard functions
+      'MunifTanjim/nui.nvim', -- To build the plugin UI
+      'nvim-telescope/telescope.nvim', -- For picking b/w different remote methods
+    },
+    config = true,
+  },
 }
