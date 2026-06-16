b1428ba7ca7a2e2f0a8f0f28a039d933daaa1fba
Frank <25704248+rakuzen25@users.noreply.github.com>
2026-06-16 09:55:52 -0500

    fix(nvim): pin treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -955,6 +955,8 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    pin = true,
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
